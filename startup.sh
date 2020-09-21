51b4dd2db48dd67d0327353a7c1975e423e51534
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2020-09-21 13:50:21 +0200

    Add copy of inputrc

--- startup.sh
+++ startup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 # Script to facilitate startup for new system
 
-sudo apt install vim
-sudo apt install curl
-sudo apt install git
 sudo apt-get update
+sudo apt install git
+sudo apt install curl
 sudo apt-get upgrade
 
 # miktex and texlive
 sudo apt-get install texlive-science
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
 
 # create bash alias file
 touch ~/.bash_aliases
@@ -20,12 +15,24 @@ touch ~/.bash_aliases
 # copy vimrc and bash in right places
 cp .vimrc ~/.vimrc
 cp .bashrc ~/.bashrc
+cp .bashrc ~/.inputrc
 cp .bash_profile ~/.bash_profile
 cp .profile ~/.profile
 
 git config --global user.name "Dieuwke Hupkes"
 git config --global user.email dieuwkehupkes@gmail.com
 git config --global core.editor "vim"
+
+# Vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# solarized
+cd ~/.vim/bundle
+git clone https://github.com/altercation/vim-colors-solarized.git
 
 # to generate new ssh key type
 ########################################
